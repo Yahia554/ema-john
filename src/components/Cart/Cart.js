85107ff158458ccc65234071a7fc90a0a8aa78bb
yahia salman <mdyahiasalman624@gmail.com>
2022-10-26 03:14:52 +0600

    shop responsive

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -11,7 +11,6 @@ const Cart = (props) => {
     total = total + product.price * product.quantity;
     shipping = shipping + product.shipping;
   }
-  console.log(shipping);
   const tax = parseFloat((total * 0.1).toFixed(2));
   const grandTotal = total + shipping + tax;
   return (
