0b4e338186d45ddfa04abe15d0e856b670cc1c52
yahia salman <mdyahiasalman624@gmail.com>
2022-12-30 17:13:36 +0600

    git-live-link added

---
+++ src/components/NotFound/NotFound.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./NotFound.css";
+
+const NotFound = () => {
+  return (
+    <div className="notfound">
+      <div>
+        <h1>404</h1>
+        <h3>Result Not Found</h3>
+      </div>
+    </div>
+  );
+};
+
+export default NotFound;
